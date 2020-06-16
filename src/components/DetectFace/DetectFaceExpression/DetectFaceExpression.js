5793f971db26a34c6e9691b8db1d6b779c393599
AleksaCupic <aleksa.cupic@yahoo.com>
2020-06-16 14:51:56 -0400

    Switched to useReducer in DetectFaceExpression Component

--- src/components/DetectFace/DetectFaceExpression/DetectFaceExpression.js
+++ src/components/DetectFace/DetectFaceExpression/DetectFaceExpression.js
@@ -1,50 +1,90 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState, useReducer } from 'react'
 import { MyContext } from '../../../Context/Context';
 
 import styles from './DetectFaceExpression.module.css'
 
+const detectFaceExpressionReducer=(state, action)=>{
+    switch (action.type){
+        case 'success':
+            return{
+                loading: false,
+                prediction: action.payload.prediction,
+                confidence: action.payload.confidence
+            }
+
+    }
+}
+
+const initialState={
+    prediction: '',
+    loading: true,
+    confidence: '',
+}
+
+
 /* eslint-disable */
 const DetectFaceExpression =()=>{
 
-    const[prediction, setPrediction]=useState(null)
-    const[loading, setLoading]=useState(true)
-    const[confidence, setConfidence]=useState(null)
+    const[state, dispatch]=useReducer(detectFaceExpressionReducer,initialState);
+
+    const{prediction, loading, confidence}=state
+
+    // const[prediction, setPrediction]=useState(null)
+    // const[loading, setLoading]=useState(true)
+    // const[confidence, setConfidence]=useState(null)
 
     const predictionLabels=['Angry','Happy','Sad','Surprised']
 
     const predictFacialExpression=()=>{
-        const image=document.getElementById("img")
-        predictModel(image);
+        const image= document.getElementById("img")
+        if(loading){
+            predictModel(image);
+        }
     }
 
 
     const predictModel=async(image)=>{
-        if(loading){
-            console.log("Loading model...")
-            let model=await tf.loadLayersModel('/tfModel/model.json');
-            let offset= tf.scalar(255.0);
-            let myTensor= tf.browser.fromPixels(image)
-            .resizeBilinear([64, 64]) // change the image size
-            .toFloat() 
-            .div(offset)            
-            .expandDims(0);    
-            
-            console.log("Predicting image...")
+        
+        console.log("Loading model...");
 
-            //    0      1      2       3
-            //'Angry','Happy','Sad','Surprised'
-            let predictions = await model.predict(myTensor).data();
-            
-            console.log(predictions);
+        const model=await tf.loadLayersModel('/tfModel/model.json');
+        
+        const myTensor=augumentPhoto(image)
+        
+        console.log("Predicting image...");
 
-            //Get index of heighest value in predicitions (Float32Array)
-            const predictedEmotion=predictions.reduce((iMax, x, i, arr) => x > arr[iMax] ? i : iMax, 0);
+        
+        //'Angry','Happy','Sad','Surprised'
+        let predictions = await model.predict(myTensor).data();
+        
+        console.log(predictions);
 
-            setLoading(false);
-            setPrediction(predictedEmotion);
-            setConfidence((predictions[predictedEmotion]*100).toFixed(2));            
-        }
+        //Get index of heighest value in predicitions (Float32Array)
+        const predictedEmotion=predictions
+        .reduce((iMax, x, i, arr) => x > arr[iMax] ? i : iMax, 0);
+
+        dispatch(
+            {type: 'success', 
+            payload: {
+                prediction: predictedEmotion, 
+                confidence: (predictions[predictedEmotion]*100).toFixed(2)
+            }
+        });            
+        
     }
+
+    const augumentPhoto=(image)=>{
+        
+        let offset= tf.scalar(255.0);
+        let myTensor=  tf.browser.fromPixels(image)
+        .resizeBilinear([64, 64]) // resize image
+        .toFloat() 
+        .div(offset)            
+        .expandDims(0); 
+
+        return myTensor
+    }
+
 
     const styledConfidence=()=>{
         if(confidence>85){
